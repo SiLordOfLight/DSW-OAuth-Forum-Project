dfd046765121d8dd71b70a4c973b20f090d5e3b6
Jake Trimble <jatnerd@gmail.com>
2019-02-14 19:06:20 -0800

    Fix security

--- webapp.py
+++ webapp.py
@@ -13,7 +13,7 @@ app.debug = True #Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 admins = os.environ['admins']
 
@@ -165,7 +165,7 @@ def replyPost():
 #redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
 def login():
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
